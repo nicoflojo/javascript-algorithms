67401f5d32b46d1f8cf974c48450f70b84f29bd9
nico <nico@pop-os.localdomain>
2019-11-26 16:08:38 +0700

    cleans up queues params

--- src/data-structures/priority-queue/PriorityQueue.js
+++ src/data-structures/priority-queue/PriorityQueue.js
@@ -16,8 +16,6 @@ export default class PriorityQueue extends MinHeap {
     this.compare = new Comparator(this.comparePriority.bind(this));
   }
 
-  /**
-   * Add item to the priority queue.
    * @param {*} item - item we're going to add to the queue.
    * @param {number} [priority] - items priority.
    * @return {PriorityQueue}
@@ -28,56 +26,26 @@ export default class PriorityQueue extends MinHeap {
     return this;
   }
 
-  /**
-   * Remove item from priority queue.
-   * @param {*} item - item we're going to remove.
-   * @param {Comparator} [customFindingComparator] - custom function for finding the item to remove
-   * @return {PriorityQueue}
-   */
   remove(item, customFindingComparator) {
     super.remove(item, customFindingComparator);
     this.priorities.delete(item);
     return this;
   }
 
-  /**
-   * Change priority of the item in a queue.
-   * @param {*} item - item we're going to re-prioritize.
-   * @param {number} priority - new item's priority.
-   * @return {PriorityQueue}
-   */
   changePriority(item, priority) {
     this.remove(item, new Comparator(this.compareValue));
     this.add(item, priority);
     return this;
   }
 
-  /**
-   * Find item by ite value.
-   * @param {*} item
-   * @return {Number[]}
-   */
   findByValue(item) {
     return this.find(item, new Comparator(this.compareValue));
   }
 
-  /**
-   * Check if item already exists in a queue.
-   * @param {*} item
-   * @return {boolean}
-   */
   hasValue(item) {
     return this.findByValue(item).length > 0;
   }
 
-  /**
-   * Compares priorities of two items.
-   * @param {*} a
-   * @param {*} b
-   * @return {number}
-   */
-  comparePriority(a, b) {
-    if (this.priorities.get(a) === this.priorities.get(b)) {
       return 0;
     }
     return this.priorities.get(a) < this.priorities.get(b) ? -1 : 1;
--- src/data-structures/queue/Queue.js
+++ src/data-structures/queue/Queue.js
@@ -9,17 +9,10 @@ export default class Queue {
     this.linkedList = new LinkedList();
   }
 
-  /**
-   * @return {boolean}
-   */
   isEmpty() {
     return !this.linkedList.head;
   }
 
-  /**
-   * Read the element at the front of the queue without removing it.
-   * @return {*}
-   */
   peek() {
     if (!this.linkedList.head) {
       return null;
@@ -28,29 +21,15 @@ export default class Queue {
     return this.linkedList.head.value;
   }
 
-  /**
-   * Add a new element to the end of the queue (the tail of the linked list).
-   * This element will be processed after all elements ahead of it.
-   * @param {*} value
-   */
   enqueue(value) {
     this.linkedList.append(value);
   }
 
-  /**
-   * Remove the element at the front of the queue (the head of the linked list).
-   * If the queue is empty, return null.
-   * @return {*}
-   */
   dequeue() {
     const removedHead = this.linkedList.deleteHead();
     return removedHead ? removedHead.value : null;
   }
 
-  /**
-   * @param [callback]
-   * @return {string}
-   */
   toString(callback) {
     // Return string representation of the queue's linked list.
     return this.linkedList.toString(callback);
